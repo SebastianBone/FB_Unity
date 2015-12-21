1104e74c1b760ac77265fb8368d1c2e616349682
Sebastian Bohn <s.bohn@nodapo.de>
2015-12-21 16:36:14 +0100

    Comments

--- Flappy Bird/Assets/Scripts/PipeAnimation.cs
+++ Flappy Bird/Assets/Scripts/PipeAnimation.cs
@@ -9,24 +9,17 @@ public class PipeAnimation : MonoBehaviour {
 	Vector2 position;
 
 	//Geschwindigkeit der Hindernisse
-	float speed = 0.1f;
+	public float speed = 0.1f;
 
 	// Startfunktion wird initial aufgerufen
 	void Start(){
 		position = transform.position;
 		InvokeRepeating("animate",0,0.033f);
 	}
-
-	/// <summary>
-	/// Updatefunktion wird pro Frame aufgerufen
-	/// </summary>
-	void Update () {
 		
-		//position.x -= speed;
-		//transform.position = position;
-
-	}
-
+	/// <summary>
+	/// Animate the Pipe
+	/// </summary>
 	void animate(){
 		position.x -= speed;
 		transform.position = position;
